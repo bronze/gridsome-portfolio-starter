8433dbaac21a4cc49332a3be7197fd967a48d473
Carlos Bronze <carlosbronze@gmail.com>
2019-10-21 13:19:30 -0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -148,14 +148,14 @@
 </template>
 
 <script>
-$("#contact").submit(function(e) {
-  e.preventDefault();
-
-  var $form = $(this);
-  $.post($form.attr("action"), $form.serialize()).then(function() {
-    alert("Thank you!");
-  });
-});
+// $("#contact").submit(function(e) {
+//   e.preventDefault();
+// 
+//   var $form = $(this);
+//   $.post($form.attr("action"), $form.serialize()).then(function() {
+//     alert("Thank you!");
+//   });
+// });
 export default {
   metaInfo: {
     title: 'Home'
