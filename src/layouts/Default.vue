1905394cf4842ed3cefddaec7d85d7701a253d90
Carlos Bronze <170220+bronze@users.noreply.github.com>
2019-12-24 12:04:06 -0300

    dark mode working

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,7 @@
     <header class="border-t-14 border-red-700">
       <nav class="container mx-auto flex flex-wrap justify-between items-center py-8">
         <div>
-          <g-link v-if="darkTheme" to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
+          <g-link v-if="theme === 'light'" to="/"><g-image src="../../static/logo.svg" class="w-40" alt="logo" /></g-link>
           <g-link v-else to="/"><g-image src="../../static/logo_dark_mode.svg" class="w-40" alt="logo" /></g-link>
         </div>
         <div class="block lg:hidden">
